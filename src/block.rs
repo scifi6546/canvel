b04738b742f9e4dd77d86ef562e1b41a4accbf3e
BuildTools <nickalexeev@protonmail.com>
2020-07-19 07:53:21 -0800

    got tests working

--- src/block.rs
+++ src/block.rs
@@ -1,16 +1,15 @@
 use std::collections::HashMap;
 use std::fs::File;
 use std::io::SeekFrom;
-use std::io::{Seek, Write,Read};
-use std::ops::Index; 
-use std::fs::OpenOptions;
+use std::io::{Read, Seek, Write};
+use std::ops::Index;
 pub type Id = u32;
 pub trait Serialize {
     fn size_of() -> usize;
     fn to_bytes(&self) -> Vec<u8>;
     fn from_bytes(bytes: Vec<u8>) -> Self;
 }
-pub struct Block<'a,Row: Serialize+Clone> {
+pub struct Block<Row: Serialize + Clone> {
     //Block structure
     //```
     //
@@ -21,20 +20,16 @@ pub struct Block<'a,Row: Serialize+Clone> {
     //BIT_MAP
     //ROWS
     //(get_block_size)*(index+1)-size_of: last row
-    file: &'a mut File,
     data: HashMap<Id, Row>,
     index_start: Id, //index of smallest element
 }
-impl <'a,Row:Serialize+Clone+std::marker::Sized> Index<&Id> for Block<'a,Row>{
+impl<Row: Serialize + Clone + std::marker::Sized> Index<(&File, &Id)> for Block<Row> {
     type Output = Row;
-    fn index(&self,i:&Id)->&Self::Output{
-        self.get(i.clone())
-
+    fn index(&self, i: (&File, &Id)) -> &Self::Output {
+        self.get(i.0, i.1.clone())
     }
-
-
 }
-impl<'a,Row: Serialize + Clone+std::marker::Sized> Block<'a,Row> {
+impl<Row: Serialize + Clone + std::marker::Sized> Block<Row> {
     pub const BLOCK_SIZE: u32 = 4096;
     fn get_bitmap_size() -> usize {
         let mut bitmap_size: usize = Self::BLOCK_SIZE as usize / 8;
@@ -44,25 +39,25 @@ impl<'a,Row: Serialize + Clone+std::marker::Sized> Block<'a,Row> {
         }
         return bitmap_size;
     }
-    fn get_block_size() -> usize {
-        let mut bitmap_size: usize = Self::get_bitmap_size();
+    pub fn get_block_size() -> usize {
+        let bitmap_size: usize = Self::get_bitmap_size();
         return bitmap_size + (Self::BLOCK_SIZE as usize) * Row::size_of();
     }
     fn get_file_start(&self) -> usize {
         (self.index_start / Self::BLOCK_SIZE) as usize * Self::get_block_size()
     }
 
-    pub fn get(&self, id: Id) -> &Row {
+    pub fn get(&self, _file: &File, id: Id) -> &Row {
         self.data.get(&id).unwrap()
     }
-    pub fn contains(&self, id: Id) -> bool {
+    pub fn contains(&self, _file: &File, id: Id) -> bool {
         self.data.contains_key(&id)
     }
-    pub fn len(&self)->usize{
+    pub fn len(&self) -> usize {
         self.data.len()
     }
     //removes id
-    pub fn remove(&mut self,id:Id){
+    pub fn remove(&mut self, file: &mut File, id: Id) {
         self.data.remove(&id);
         //first part make bitmap
 
@@ -94,17 +89,15 @@ impl<'a,Row: Serialize + Clone+std::marker::Sized> Block<'a,Row> {
         if self.data.contains_key(&(start + 7)) {
             bit += 0b1000_0000;
         }
-        self.file
-            .seek(SeekFrom::Start(
-                self.get_file_start() as u64 + id as u64 / 8,
-            ))
-            .ok()
-            .unwrap();
-        self.file.write(&[bit]);
-
+        file.seek(SeekFrom::Start(
+            self.get_file_start() as u64 + id as u64 / 8,
+        ))
+        .ok()
+        .unwrap();
+        file.write(&[bit]);
     }
     //insert row into db
-    pub fn insert(&mut self, id: Id, data: Row) {
+    pub fn insert(&mut self, file: &mut File, id: Id, data: Row) {
         self.data.insert(id, data.clone());
         //first part make bitmap
 
@@ -136,69 +129,66 @@ impl<'a,Row: Serialize + Clone+std::marker::Sized> Block<'a,Row> {
         if self.data.contains_key(&(start + 7)) {
             bit += 0b1000_0000;
         }
-        self.file
-            .seek(SeekFrom::Start(
-                self.get_file_start() as u64 + id as u64 / 8,
-            ))
-            .ok()
-            .unwrap();
-        self.file.write(&[bit]);
+        file.seek(SeekFrom::Start(
+            self.get_file_start() as u64 + id as u64 / 8,
+        ))
+        .ok()
+        .unwrap();
+        file.write(&[bit]);
 
         //Next get index of data
         let row_index =
             self.get_file_start() + Self::get_bitmap_size() + Row::size_of() * id as usize;
-        self.file.seek(SeekFrom::Start(row_index as u64));
-        self.file.write(&data.to_bytes());
+        file.seek(SeekFrom::Start(row_index as u64));
+        file.write(&data.to_bytes());
         //procedure: Figure out right part of file and then write bytes to it
         // and then insert data into hashmap
     }
-    fn load_from_disk(file:&'a mut File,id_start:Id)->Self{
+    fn load_from_disk(file: &mut File, id_start: Id) -> Self {
         //first load bitmap
         let mut hash = HashMap::new();
-        let mut bitmap= vec![0;Self::get_bitmap_size()];
+        let mut bitmap = vec![0; Self::get_bitmap_size()];
         let len = file.read(&mut bitmap).unwrap();
-        assert_eq!(len,Self::get_bitmap_size());
-        let mut row_data=vec![0;Self::BLOCK_SIZE as usize*Row::size_of()];
+        assert_eq!(len, Self::get_bitmap_size());
+        let mut row_data = vec![0; Self::BLOCK_SIZE as usize * Row::size_of()];
         let len = file.read(&mut row_data).unwrap();
-        assert_eq!(len,Self::BLOCK_SIZE as usize*Row::size_of());
-        for i in 0..Self::BLOCK_SIZE{
-            let byte = bitmap[i as usize/8];
-            let present = (byte>>i%8)&0b01;
-            if present==1{
+        assert_eq!(len, Self::BLOCK_SIZE as usize * Row::size_of());
+        for i in 0..Self::BLOCK_SIZE {
+            let byte = bitmap[i as usize / 8];
+            let present = (byte >> i % 8) & 0b01;
+            if present == 1 {
                 //if the row is present add it to hashmap
-                let mut data:Vec<u8> = vec![];
+                let mut data: Vec<u8> = vec![];
                 data.reserve(Row::size_of());
-                for j in 0..Row::size_of(){
-                    data.push(row_data[i as usize*Row::size_of()+j]);
+                for j in 0..Row::size_of() {
+                    data.push(row_data[i as usize * Row::size_of() + j]);
                 }
-                hash.insert(i+id_start,Row::from_bytes(data));
-
+                hash.insert(i + id_start, Row::from_bytes(data));
             }
         }
-        return Block{
-            file:file,
-            data:hash,
-            index_start:id_start
-        }
+        return Block {
+            data: hash,
+            index_start: id_start,
+        };
         //next load proper things from data segment
         //return block
     }
     //creates new zero element file
-    pub fn new(file:&'a mut File,id_start:Id)->Self{
-        let mut hash = HashMap::new();
-        let mut bitmap= vec![0;Self::get_bitmap_size()];
+    pub fn new(file: &mut File, id_start: Id) -> Self {
+        let hash = HashMap::new();
+        let mut bitmap = vec![0; Self::get_bitmap_size()];
         let len = file.write(&mut bitmap).unwrap();
-        assert_eq!(len,Self::get_bitmap_size());
-        Block{
-            file:file,
-            data:hash,
-            index_start:id_start
+        assert_eq!(len, Self::get_bitmap_size());
+        Block {
+            data: hash,
+            index_start: id_start,
         }
     }
 }
 #[cfg(test)]
 mod test {
     use super::*;
+    use std::fs::OpenOptions;
     impl Serialize for u32 {
         fn size_of() -> usize {
             4
@@ -211,29 +201,49 @@ mod test {
         }
     }
     #[test]
-    fn build_block(){
+    fn build_block() {
         {
-        let mut file = OpenOptions::new().write(true).read(true).create(true).open("test.db").ok().unwrap();
-        file.set_len(Block::<u32>::get_block_size() as u64).ok().unwrap();
-        let block = Block::<u32>::new(&mut file, 0);
+            let mut file = OpenOptions::new()
+                .write(true)
+                .read(true)
+                .create(true)
+                .open("block_test.db")
+                .ok()
+                .unwrap();
+            file.set_len(Block::<u32>::get_block_size() as u64)
+                .ok()
+                .unwrap();
+            let block = Block::<u32>::new(&mut file, 0);
+            assert_eq!(block.len(), 0);
         }
-        let mut file = OpenOptions::new().write(true).read(true).open("test.db").ok().unwrap();
-        file.set_len(Block::<u32>::get_block_size() as u64).ok().unwrap();
+        let mut file = OpenOptions::new()
+            .write(true)
+            .read(true)
+            .open("block_test.db")
+            .ok()
+            .unwrap();
+        file.set_len(Block::<u32>::get_block_size() as u64)
+            .ok()
+            .unwrap();
         let block = Block::<u32>::load_from_disk(&mut file, 0);
-        assert_eq!(block.len(),0);
-
-
+        assert_eq!(block.len(), 0);
     }
     #[test]
-    fn remove(){
-       
-        let mut file = OpenOptions::new().write(true).read(true).create(true).open("test.db").ok().unwrap();
-        file.set_len(Block::<u32>::get_block_size() as u64).ok().unwrap();
+    fn remove() {
+        let mut file = OpenOptions::new()
+            .write(true)
+            .read(true)
+            .create(true)
+            .open("block_test_remove.db")
+            .ok()
+            .unwrap();
+        file.set_len(Block::<u32>::get_block_size() as u64)
+            .ok()
+            .unwrap();
         let mut block = Block::<u32>::new(&mut file, 0);
-        block.insert(0,1);
-        assert_eq!(block.get(0),1);
-        block.remove(0);
-        assert_eq!(block.len(),0);
-        
+        block.insert(&mut file, 0, 1);
+        assert_eq!(block.get(&file, 0), &1);
+        block.remove(&mut file,0);
+        assert_eq!(block.len(), 0);
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,9 @@
-use std::collections::HashMap;
-use std::fs::OpenOptions;
 use std::fs::File;
+use std::fs::OpenOptions;
 use std::sync::{Mutex, RwLock, RwLockReadGuard, RwLockWriteGuard};
 use std::vec::Vec;
 mod block;
-use block::{Block,Serialize};
+use block::{Block, Serialize};
 
 trait DbSized {
     const SIZE: u32;
@@ -13,12 +12,12 @@ impl Serialize for Row {
     fn to_bytes(&self) -> Vec<u8> {
         self.test_data.to_le_bytes().to_vec()
     }
-    fn from_bytes(bytes: Vec<u8>)->Self {
-        Row{
-            test_data:u32::from_le_bytes([bytes[0], bytes[1], bytes[2], bytes[3]])
+    fn from_bytes(bytes: Vec<u8>) -> Self {
+        Row {
+            test_data: u32::from_le_bytes([bytes[0], bytes[1], bytes[2], bytes[3]]),
         }
     }
-    fn size_of()->usize{
+    fn size_of() -> usize {
         4
     }
 }
@@ -30,20 +29,20 @@ pub enum DBError {
     IDNotInDatabase,
 }
 type ID = u32;
-pub struct DB<'a> {
+pub struct DB {
     file: File,
-    data: RwLock<Vec<Mutex<Block<'a,Row>>>>,
+    data: RwLock<Vec<Mutex<Block<Row>>>>,
 }
 //unsafe impl Sync for DB {}
-impl<'a> DB<'a> {
+impl DB {
     const MUTEX_SIZE: u32 = 4096;
     pub fn get_row(&self, id: ID) -> Result<Row, DBError> {
-        if Self::contains_id(self.data.read().ok().unwrap(), id) {
+        if Self::contains_id(&(self.file), self.data.read().ok().unwrap(), id) {
             Ok(
                 self.data.read().ok().unwrap()[(id.clone() / Self::MUTEX_SIZE) as usize]
                     .lock()
                     .ok()
-                    .unwrap()[&id]
+                    .unwrap()[(&(self.file), &id)]
                     .clone(),
             )
         } else {
@@ -52,46 +51,67 @@ impl<'a> DB<'a> {
     }
     //creates new database with rows.
     //overwrites contents
-    pub fn from_rows(data: &Vec<Row>,database_path:&str) -> (Self, Vec<ID>) {
-
-        let mut file = OpenOptions::new().write(true).read(true).create(true).open(database_path).ok().unwrap();
+    pub fn from_rows(data: &Vec<Row>, database_path: &str) -> (Self, Vec<ID>) {
+        let mut file = OpenOptions::new()
+            .write(true)
+            .read(true)
+            .create(true)
+            .open(database_path)
+            .ok()
+            .unwrap();
         let mut blocks = vec![];
         //increasing block size by one
-        let mut num_blocks = data.len()/Block::<Row>::BLOCK_SIZE as usize;
-        if data.len()%Block::<Row>::BLOCK_SIZE as usize!=0{
-            num_blocks+=1;
+        let mut num_blocks = data.len() / Block::<Row>::BLOCK_SIZE as usize;
+        if data.len() % Block::<Row>::BLOCK_SIZE as usize != 0 {
+            num_blocks += 1;
         }
         blocks.reserve(num_blocks);
-        for i in 0..num_blocks{
-            blocks.push(Mutex::new(Block::new(&mut file,i as u32*Block::<Row>::BLOCK_SIZE)));
+        for i in 0..num_blocks {
+            blocks.push(Mutex::new(Block::new(
+                &mut file,
+                i as u32 * Block::<Row>::BLOCK_SIZE,
+            )));
         }
-        let mut db = DB{
-            file:file,
-            data:RwLock::new(blocks)
+        let mut db = DB {
+            file: file,
+            data: RwLock::new(blocks),
         };
-        let ids = data.iter().map(|r|{db.insert(r.clone())}).collect();
-        return (db,ids);
-
-
+        let ids = data.iter().map(|r| db.insert(r.clone())).collect();
+        return (db, ids);
     }
     //increases capacity of db by size
-    fn grow<'b>(data: &'b mut RwLockWriteGuard<'b, Vec<Mutex<Block<'a,Row>>>>,size:usize){
-        unimplemented!();
+    //if new block is nesaccary a new block is added, if it is not nessacary
+    //function does nothing
+    fn grow(file:&mut File,data: &mut RwLockWriteGuard<'_, Vec<Mutex<Block<Row>>>>, size: usize) {
+        //first lets find the len
+        let BLOCK_SIZE = Block::<Row>::BLOCK_SIZE;
+        let len:usize = data.iter().map(|b|{b.lock().ok().unwrap().len()}).sum();
+        let resize_amount:usize = (len as usize+size as usize)/BLOCK_SIZE as usize-len/BLOCK_SIZE as usize;
+        if resize_amount!=0{
+            let mut new_block_count = (len+size)/BLOCK_SIZE as usize;
+            if (len+size)%BLOCK_SIZE as usize!=0{
+                new_block_count+=1;
+            }
+            let old_block_count = data.len();
+            let size = Block::<Row>::get_block_size()*(new_block_count as usize);
+            file.set_len(size as u64).ok().unwrap();
+            for i in old_block_count..new_block_count{
+                data.push(Mutex::new(Block::new(file,i as u32*BLOCK_SIZE as u32)));
+            }
+        }
     }
     //Returns id and lock to current block to ensure that id is not taken
-    fn get_free_id<'b>(
-        data: &'b mut RwLockWriteGuard<'b, Vec<Mutex<Block<'a,Row>>>>,
-    ) -> (
-        ID,
-        std::sync::MutexGuard<'b, Block<'a,Row>>,
-    ) {
+    fn get_free_id<'a, 'b>(
+        file: &mut File,
+        data: &'a mut RwLockWriteGuard<'b, Vec<Mutex<Block<Row>>>>,
+    ) -> (ID, std::sync::MutexGuard<'a, Block<Row>>) {
         {
             for i in 0..data.len() {
                 //If there is a free id somewhere
                 if data[i].lock().ok().unwrap().len() < Self::MUTEX_SIZE as usize {
                     //looking for free index
                     for j in (i as u32 * Self::MUTEX_SIZE)..(i as u32 + 1) * Self::MUTEX_SIZE {
-                        if data[i].lock().ok().unwrap().contains(j) == false {
+                        if data[i].lock().ok().unwrap().contains(file, j) == false {
                             return (j, data[i].lock().ok().unwrap());
                         }
                     }
@@ -100,24 +120,22 @@ impl<'a> DB<'a> {
                 }
             }
         }
-        Self::grow(data,1);
+        Self::grow(file,data, 1);
         let lock = data[data.len() - 1].lock().ok().unwrap();
         let id = (data.len() as u32 - 1) * Self::MUTEX_SIZE;
         return (id, lock);
     }
     //inserts new row into database.
-    pub fn insert(&mut self, row: Row)->ID {
+    pub fn insert(&mut self, row: Row) -> ID {
         let mut write_lock = self.data.write().ok().unwrap();
-        let (id, mut block) = Self::get_free_id(&mut write_lock);
-        block.insert(id, row);
+        let (id, mut block) = Self::get_free_id(&mut (self.file), &mut write_lock);
+        block.insert(&mut (self.file), id, row);
         return id;
     }
     /// Update row. Undefined if row is not already present
     fn update_inplace_row(
-        data: RwLockReadGuard<
-            '_,
-            std::vec::Vec<std::sync::Mutex<Block<'a, Row>>>,
-        >,
+        file: &mut File,
+        data: RwLockReadGuard<'_, std::vec::Vec<std::sync::Mutex<Block<Row>>>>,
         id: ID,
         row: Row,
     ) {
@@ -125,24 +143,24 @@ impl<'a> DB<'a> {
             .lock()
             .ok()
             .unwrap()
-            .insert(id, row);
+            .insert(file, id, row);
     }
-    fn contains_id(data: RwLockReadGuard<'_, Vec<Mutex<Block<Row>>>>, id: ID) -> bool {
+    fn contains_id(file: &File, data: RwLockReadGuard<'_, Vec<Mutex<Block<Row>>>>, id: ID) -> bool {
         if ((id / Self::MUTEX_SIZE) as usize) < data.len() {
             return data[(id / Self::MUTEX_SIZE) as usize]
                 .lock()
                 .ok()
                 .unwrap()
-                .contains(id);
+                .contains(file, id);
         } else {
             return false;
         }
     }
     ///if id is not already present inserts new row. If id is present the row is overwritten.
     pub fn update_row(&mut self, id: ID, row: Row) -> Result<(), DBError> {
-        if Self::contains_id(self.data.read().ok().unwrap(), id) {
+        if Self::contains_id(&(self.file), self.data.read().ok().unwrap(), id) {
             let read_lock = self.data.read().ok().unwrap();
-            Self::update_inplace_row(read_lock, id, row);
+            Self::update_inplace_row(&mut (self.file), read_lock, id, row);
             Ok(())
         } else {
             Err(DBError::IDNotInDatabase)
@@ -150,11 +168,11 @@ impl<'a> DB<'a> {
     }
     ///Deletes row from db
     pub fn delete_row(&mut self, id: ID) -> Result<(), DBError> {
-        if Self::contains_id(self.data.read().ok().unwrap(), id) {
+        if Self::contains_id(&(self.file), self.data.read().ok().unwrap(), id) {
             self.data.read().ok().unwrap()[(id / Self::MUTEX_SIZE) as usize]
                 .lock()
                 .unwrap()
-                .remove(id);
+                .remove(&mut (self.file), id);
             Ok(())
         } else {
             Err(DBError::IDNotInDatabase)
@@ -169,25 +187,23 @@ mod tests {
     }
     #[test]
     fn make_db() {
-        let db0 = DB::from_rows(&make_rows(0),"test1.db");
-        let db1 = DB::from_rows(&make_rows(4096),"test2.db");
-        let db2 = DB::from_rows(&make_rows(5000),"test3.db");
+        DB::from_rows(&make_rows(0), "test1.db");
+        DB::from_rows(&make_rows(4096), "test2.db");
+        DB::from_rows(&make_rows(5000), "test3.db");
         assert_eq!(2 + 2, 4);
     }
     #[test]
     fn make_and_get() {
-        let (db2, ids) = DB::from_rows(&make_rows(5000),"test4.db");
+        let (db2, ids) = DB::from_rows(&make_rows(5000), "test4.db");
         for i in 0..5000 {
             assert_eq!(db2.get_row(i).ok().unwrap().test_data, i);
         }
     }
     #[test]
     fn update_row() {
-        let (mut db2, ids) = DB::from_rows(&make_rows(5000),"test5.db");
+        let (mut db2, ids) = DB::from_rows(&make_rows(5000), "test5.db");
         for id in ids.iter() {
-            unsafe {
-                db2.update_row(id.clone(), Row { test_data: 0 });
-            }
+            db2.update_row(id.clone(), Row { test_data: 0 });
         }
         for id in ids {
             assert_eq!(db2.get_row(id).ok().unwrap().test_data, 0);
